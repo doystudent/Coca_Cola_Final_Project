40d5ed76c17dbf2b504b47b31503b870775d79ee
doystudent <matthewng08@gmail.com>
2023-02-09 21:14:20 -0500

    Finished adding La and Boston Maps

--- data-download.R
+++ data-download.R
@@ -10,3 +10,23 @@ dallas_bachelors <-
           geometry = TRUE)
 
 write_rds(dallas_bachelors, "data/dallas.rds")
+
+la_bachelors <- 
+  get_acs(geography = "tract",
+          variables = "DP02_0068P",
+          year = 2021,
+          state = "CA",
+          county = "Los Angeles",
+          geometry = TRUE)
+
+write_rds(la_bachelors, "data/la.rds")
+
+boston_bachelors <- 
+  get_acs(geography = "tract",
+          variables = "DP02_0068P",
+          year = 2021,
+          state = "MA",
+          county = "Suffolk",
+          geometry = TRUE)
+
+write_rds(boston_bachelors, "data/boston.rds")
